1f0bc0794c8d6039a7f49c40e92f8bd1b34b9ec1
Thiago Ramos <thiagorbernardo@hotmail.com>
2022-05-04 20:39:33 -0300

    feat: final script

--- run.sh
+++ run.sh
@@ -1,14 +1,23 @@
 ghdl -a alu.vhd
 ghdl -a alu_tb.vhd
 
-ghdl -a reg16bits.vhd
-ghdl -a reg16bits_tb.vhd
+ghdl -a processor.vhd
+ghdl -a processor_tb.vhd
 
 ghdl -a reg_bank.vhd
 ghdl -a reg_bank_tb.vhd
 
-ghdl -a processor.vhd
-ghdl -a processor_tb.vhd
+ghdl -a reg16bits.vhd
+ghdl -a reg16bits_tb.vhd
 
-ghdl -r processor_tb --wave=processor_tb.ghw
-gtkwave processor_tb.ghw
+ghdl -a rom.vhd
+ghdl -a rom_tb.vhd
+
+ghdl -a state_machine.vhd
+ghdl -a state_machine_tb.vhd
+
+ghdl -a uc.vhd
+ghdl -a uc_tb.vhd
+
+ghdl -r uc_tb --wave=uc_tb.ghw
+gtkwave uc_tb.ghw
